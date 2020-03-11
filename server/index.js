c51d400882766027519ad99b9972d4b127db947f
Melissa Murphy <flylittlefeather@yahoo.com>
2020-03-11 21:53:45 +0000

    Created 'products' table in wickedSales database (ran command in PGWeb), imported data (products.csv) into database (with psql console), added GET endpoint that queries database

--- server/index.js
+++ server/index.js
@@ -19,6 +19,16 @@ app.get('/api/health-check', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.get('/api/products', (req, res, next) => {
+  const sql = `
+    select "productId", "name", "price", "image", "shortDescription"
+      from "products"
+  `;
+  db.query(sql)
+    .then(result => res.json(result.rows))
+    .catch(err => next(err));
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
