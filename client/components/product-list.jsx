b2fc6adeaa9cd149c1efa96fa1ff4247f6921abd
Melissa Murphy <flylittlefeather@yahoo.com>
2020-03-12 22:35:09 +0000

    completedfront-end interface that makes request to back end (to 'fetch' data for products) and displays product 'cards'

--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -1,20 +1,5 @@
 import React from 'react';
-
-function ProductListItem(props) {
-  const product = props.product;
-  return (
-    <div className="col-4 mb-4">
-      <div className="card shadow-sm">
-        <img src={product.image} alt={product.name} className="card-img-top"/>
-        <div className="card-body">
-          <h5 className="card-title"> {product.name} </h5>
-          <p className="card-text text-muted"> {`$${(product.price / 100).toFixed(2)}`} </p>
-          <p className="card-text"> {product.shortDescription} </p>
-        </div>
-      </div>
-    </div>
-  );
-}
+import ProductListItem from './product-list-item';
 
 class ProductList extends React.Component {
   constructor(props) {
