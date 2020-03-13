508c43c4a6a61fc818c7c3fd0a4937094dc4ec52
Melissa Murphy <flylittlefeather@yahoo.com>
2020-03-13 03:33:11 +0000

    Added endpoint to handle GET requests by querying the database and returning appropriate status code (200, 404, or 500)

--- server/index.js
+++ server/index.js
@@ -29,6 +29,32 @@ app.get('/api/products', (req, res, next) => {
     .catch(err => next(err));
 });
 
+//
+app.get('/api/products/:productId', (req, res, next) => {
+  const productId = req.params.productId;
+  const paramz = [productId];
+  // Paramaterized query: Rather than concatenate params right into the sql command, pass them (as 2nd argument in .query) in array to the PostgreSQL server,
+  // to screen through "battle-tested parameter substitution code"
+
+  const sql = `
+    select *
+      from "products"
+      where "productId" = $1
+  `;
+  db.query(sql, paramz)
+    .then(result => {
+      // Check if columns were found at the requested row (i.e. under that productId)
+      // Note if no object found for that row, result.rows will be an empty array (truthy); results.rows[0] will be undefined (falsy)
+      if (result.rows[0]) {
+        res.json(result.rows[0]);
+      } else {
+        next(new ClientError(`No product found with Id ${productId}`, 404)); // creates a client error (see 'client-error.js') and passes in as argument to next() method of express if the product wasn't found
+      }
+    })
+    .catch(err => next(err)); // Something goes wrong with the query and the Promise settles to rejected "500 Internal Server Error server error response code indicates that the server encountered an unexpected condition that prevented it from fulfilling the request."
+  // "The Promise returned by catch() is rejected if onRejected [callback] throws an error or returns a Promise which is itself rejected; otherwise, it is resolved."
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
