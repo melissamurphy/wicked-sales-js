d8bda8ac805e2c36e59e19c38f851eb4b6b77858
Melissa Murphy <flylittlefeather@yahoo.com>
2020-03-17 16:33:30 +0000

    changing branches

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './header';
 import ProductList from './product-list';
+import ProductDetails from './product-details';
 
 export default class App extends React.Component {
 
@@ -8,8 +9,9 @@ export default class App extends React.Component {
     return (
       <div>
         <Header />
+        <ProductDetails />
         <div>
-          <ProductList />
+          {/* <ProductList /> */}
         </div>
       </div>
     );
---
+++ client/components/product-details.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+export default class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null
+    };
+  }
+
+  componentDidMount() {
+    this.getProductDetails(1);
+  }
+
+  getProductDetails(productId) {
+    fetch(`/api/products/${productId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(productDetails => this.setState({ product: productDetails }));
+  }
+
+  render() {
+    const product = this.state.product;
+    if (!product) {
+      return null;
+    }
+    return (
+      <div className="container my-5">
+        <div className="row">
+          <div className="column">
+            <div className="card px-3 bg-light">
+              <p className="text-muted pt-3">{'<'} Back to catalog</p>
+              <div className="row">
+                <div className="column">
+                  <img src={product.image} alt={product.name}/>
+                </div>
+                <div className="column">
+                  <h5 className="card-title">{product.name}</h5>
+                  <p className="card-text text-muted">{`$${(product.price / 100).toFixed(2)}`}</p>
+                  <p className="card-text">{product.shortDescription}</p>
+                </div>
+              </div>
+              <div className="row">
+                {product.longDescription}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+}
