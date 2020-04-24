40bafcbc4dbe34ac912805586e4f30dd56c6880a
Melissa Murphy <flylittlefeather@yahoo.com>
2020-04-24 06:36:40 +0000

    Deleted console log statement

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -17,7 +17,6 @@ export default class App extends React.Component {
   }
 
   render() {
-    // console.log('this.state.view.params', this.state.view.params);
     if (this.state.view.name === 'catalog') {
       return (
         <div>
